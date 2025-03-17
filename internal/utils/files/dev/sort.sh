9e7d39bfc3ec6bc1a9790690942f87a220324ed9
Chocapikk <balgogan@protonmail.com>
2025-03-17 18:36:10 +0100

    Add more plugins

--- internal/utils/files/dev/sort.sh
+++ internal/utils/files/dev/sort.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-jq -cn '[inputs | to_entries[] | {plugin: .key, endpoint: .value[]} ]' scanned_plugins.json > endpoints_by_plugin.json
+jq -s '[.[] | to_entries[] | {plugin: .key, endpoint: .value[]} ]' scanned_plugins.json > endpoints_by_plugin.json
 
 jq '[group_by(.endpoint)[] | select(length > 1) | .[0].endpoint]' endpoints_by_plugin.json > common_endpoints.json
 
-jq --slurpfile common common_endpoints.json '
-  to_entries
-  | map(
-      (.value - $common) as $filtered
-      | if ($filtered | length) > 0 then
-          {(.key): $filtered}
-        else
-          {(.key): .value}
-        end
-    )
-  | add
-  | to_entries[]
-  | {(.key): .value}
-' -c scanned_plugins.json > cleaned_file.json
+jq -s --slurpfile common common_endpoints.json '
+  map(
+    to_entries[0] as $entry |
+    $entry.value as $orig |
+    ($orig - $common[0]) as $filtered |
+    if ($filtered | length) > 0 then
+      {($entry.key): $filtered}
+    else
+      {($entry.key): $orig}
+    end
+  )
+  | .[]
+' -c scanned_plugins.json > cleaned_file.jsonl
 
 cat common_endpoints.json
 
